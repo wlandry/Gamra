c00fc90dd0a0e8507f656b16623865ccdda74d16
Walter Landry <wlandry@caltech.edu>
2011-02-13 11:45:47 -0800

    Fix some sign errors in quad interpolate

--- quad_offset_interpolate.h
+++ quad_offset_interpolate.h
@@ -13,17 +13,17 @@ inline void quad_offset_interpolate(const double &plus, const double &center,
                                     double &fine_plus, double &fine_minus)
 {
   const double d_plus=plus-center;
-  const double d_minus=center-minus;
+  const double d_minus=minus-center;
 
   fine_plus=center + (5*d_plus - 3*d_minus)/32;
-  fine_minus=center + (3*d_plus - 5*d_minus)/32;
+  fine_minus=center + (5*d_minus - 3*d_plus)/32;
 }
 
 inline double quad_offset_interpolate(const double &plus, const double &center,
                                       const double &minus)
 {
   const double d_plus=plus-center;
-  const double d_minus=center-minus;
+  const double d_minus=minus-center;
 
   return center + (5*d_plus - 3*d_minus)/32;
 }
