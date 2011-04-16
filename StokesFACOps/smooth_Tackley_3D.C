61d590a3560762b18df525b1efd95d2fcea7b906
Walter Landry <wlandry@caltech.edu>
2011-04-15 20:30:39 -0700

    Make smooth_Tackley_3D do 4 times as many sweeps on the coarse level, rather than 2 times

--- StokesFACOps/smooth_Tackley_3D.C
+++ StokesFACOps/smooth_Tackley_3D.C
@@ -67,7 +67,7 @@ void SAMRAI::solv::StokesFACOps::smooth_Tackley_3D
   const hier::Index ip(1,0,0), jp(0,1,0), kp(0,0,1);
   const hier::Index pp[]={ip,jp,kp};
   bool converged = false;
-  for (int sweep=0; sweep < num_sweeps*(1<<(d_ln_max-ln)) && !converged;
+  for (int sweep=0; sweep < num_sweeps*(1<<(2*(d_ln_max-ln))) && !converged;
        ++sweep)
     {
       maxres=0;
