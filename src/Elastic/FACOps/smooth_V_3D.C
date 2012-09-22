05bdf750481c68a2f59b7c5e815e4264d57076ee
Walter Landry <wlandry@caltech.edu>
2012-09-22 16:54:22 -0700

    Remove neumann BC stuff while smoothing.  Just fix it up at the end of each sweep.

--- src/Elastic/FACOps/smooth_V_3D.C
+++ src/Elastic/FACOps/smooth_V_3D.C
@@ -33,24 +33,6 @@ void Elastic::FACOps::smooth_V_3D
   if(!((center[ix]==pbox.lower(ix) && v(x-pp[ix])==boundary_value)
        || (center[ix]==pbox.upper(ix)+1 && v(x+pp[ix])==boundary_value)))
     {
-      /* If at the boundary, set things up so that the derivative does
-         not change. */
-      SAMRAI::hier::Index offset(0,0,0);
-      offset[ix]=2;
-      bool set_lower_boundary(false), set_upper_boundary(false);
-      double dv_lower(0), dv_upper(0);
-      if(center[ix]==pbox.lower(ix)+1
-         && !geom->getTouchesRegularBoundary(ix,0))
-        {
-          set_lower_boundary=true;
-          dv_lower=v(x-offset) - v(x);
-        }
-      if(center[ix]==pbox.upper(ix) && !geom->getTouchesRegularBoundary(ix,1))
-        {
-          set_upper_boundary=true;
-          dv_upper=v(x+offset) - v(x);
-        }
-
       double C_vx=dRm_dv_3D(cell_moduli,edge_moduli,center,center-pp[ix],
                             edge_y+pp[iz],edge_y,edge_z+pp[iy],edge_z,
                             Dx[ix],Dx[iy],Dx[iz]);
@@ -63,17 +45,6 @@ void Elastic::FACOps::smooth_V_3D
       maxres=std::max(maxres,std::fabs(delta_Rx));
 
       v(x)+=delta_Rx*theta_momentum/C_vx;
-
-      /* Set the boundary elements so that the derivative is
-         unchanged. */
-      if(set_lower_boundary)
-        {
-          v(x-offset)=v(x) + dv_lower;
-        }
-      if(set_upper_boundary)
-        {
-          v(x+offset)=v(x) + dv_upper;
-        }
     }
 }
 
