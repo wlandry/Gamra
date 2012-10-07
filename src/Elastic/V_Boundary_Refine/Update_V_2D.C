d4f5af469f899fcdc8947de0f164cdb8400c67be
Walter Landry <wlandry@caltech.edu>
2012-10-07 14:32:04 -0700

    Use a Neumann'ish BC for coarse fine tangential components.  It uses both coarse and fine values to compute the boundary values.

--- src/Elastic/V_Boundary_Refine/Update_V_2D.C
+++ src/Elastic/V_Boundary_Refine/Update_V_2D.C
@@ -55,16 +55,6 @@ void Elastic::V_Boundary_Refine::Update_V_2D
       const double v_minus=
         (-v(center-ip_s-jp) + 6*v(center-jp) + 3*v(center+ip_s-jp))/8;
                             
-
-      // const double v_center=(-v(center-ip_s) + 4*v(center)
-      //                        + 4*v(center+ip_s) - v(center+ip_s+ip_s))/6;
-
-      // const double v_plus=(-v(center-ip_s+jp) + 4*v(center+jp)
-      //                      + 4*v(center+ip_s+jp) - v(center+ip_s+ip_s+jp))/6;
-                        
-      // const double v_minus=(-v(center-ip_s-jp) + 4*v(center-jp)
-      //                       + 4*v(center+ip_s-jp) - v(center+ip_s+ip_s-jp))/6;
-
       double v_p, v_m;
       quad_offset_interpolate(v_plus,v_center,v_minus,v_p,v_m);
 
@@ -93,16 +83,8 @@ void Elastic::V_Boundary_Refine::Update_V_2D
                          << v_plus << " "
                          << v_minus << " "
                          << "\n";
-
-      // v_fine(fine)=boundary_value;
-      // if(j%2==0)
-      //   {
-      //     if(j<j_max)
-      //       v_fine(fine+jp)=boundary_value;
-      //     ++j;
-      //   }
     }
-  /* Neumann conditions for the tangential direction
+  /* Dirichlet conditions for the tangential direction
 
       i-1      i      i+1
 
@@ -132,42 +114,32 @@ void Elastic::V_Boundary_Refine::Update_V_2D
 
       SAMRAI::hier::Index jp_s(boundary_positive ? jp : -jp);
 
-      const double v_minus=
-        quad_offset_interpolate(v(center-jp_s),v(center),v(center+jp_s));
-
       if(i%2==0)
         {
-          v_fine(fine)=v_minus;
+          v_fine(fine)=(8*v(center+jp_s) + 10*v_fine(fine-jp_s)
+                        - 3*v_fine(fine-jp_s-jp_s))/15;
 
           if(i<i_max)
             {
-              /* This is a bit inefficient, because we compute v_plus
-               * twice.  Once for the in-between point, and again
-               * later for the actual point. */
-
-              const double v_plus=quad_offset_interpolate(v(center+ip-jp_s),
-                                                          v(center+ip),
-                                                          v(center+ip+jp_s));
-              const double v_plus_plus=
-                quad_offset_interpolate(v(center+ip+ip-jp_s),v(center+ip+ip),
-                                        v(center+ip+ip+jp_s));
-              const double v_minus_minus=
-                quad_offset_interpolate(v(center-ip-jp_s),
-                                        v(center-ip),
-                                        v(center-ip+jp_s));
-              if(v(center+ip+ip)==boundary_value)
+              double v_coarse;
+              if(v(center+ip+ip+jp_s)==boundary_value)
                 {
-                  v_fine(fine+ip)=(-v_minus_minus + 6*v_minus + 3*v_plus)/8;
+                  v_coarse=(-v(center+jp_s-ip) + 6*v(center+jp_s)
+                            + 3*v(center+jp_s+ip))/8;
                 }
-              else if(v(center-ip)==boundary_value)
+              else if(v(center-ip+jp_s)==boundary_value)
                 {
-                  v_fine(fine+ip)=(-v_plus_plus + 6*v_plus + 3*v_minus)/8;
+                  v_coarse=(-v(center+jp_s+ip+ip) + 6*v(center+jp_s+ip)
+                            + 3*v(center+jp_s))/8;
                 }
               else
                 {
-                  v_fine(fine+ip)=(-v_minus_minus + 9*v_minus
-                                   + 9*v_plus - v_plus_plus)/16;
+                  v_coarse=(-v(center+jp_s-ip) + 9*v(center+jp_s)
+                            + 9*v(center+jp_s+ip) - v(center+jp_s+ip+ip))/16;
                 }
+              v_fine(fine+ip)=(8*v_coarse + 10*v_fine(fine-jp_s+ip)
+                               - 3*v_fine(fine-jp_s-jp_s+ip))/15;
+
 
               /* Since we update two points on 'i' at once, we
                  increment 'i' again.  This is ok, since the box in
@@ -178,29 +150,24 @@ void Elastic::V_Boundary_Refine::Update_V_2D
         }
       else
         {
-          const double v_plus=quad_offset_interpolate(v(center+ip-jp_s),
-                                                      v(center+ip),
-                                                      v(center+ip+jp_s));
-          const double v_plus_plus=
-            quad_offset_interpolate(v(center+ip+ip-jp_s),v(center+ip+ip),
-                                    v(center+ip+ip+jp_s));
-          const double v_minus_minus=
-            quad_offset_interpolate(v(center-ip-jp_s),
-                                    v(center-ip),
-                                    v(center-ip+jp_s));
-          if(v(center+ip+ip)==boundary_value)
+          double v_coarse;
+          if(v(center+ip+ip+jp_s)==boundary_value)
             {
-              v_fine(fine)=(-v_minus_minus + 6*v_minus + 3*v_plus)/8;
+              v_coarse=(-v(center+jp_s-ip) + 6*v(center+jp_s)
+                        + 3*v(center+jp_s+ip))/8;
             }
-          else if(v(center-ip)==boundary_value)
+          else if(v(center-ip+jp_s)==boundary_value)
             {
-              v_fine(fine)=(-v_plus_plus + 6*v_plus + 3*v_minus)/8;
+              v_coarse=(-v(center+jp_s+ip+ip) + 6*v(center+jp_s+ip)
+                        + 3*v(center+jp_s))/8;
             }
           else
             {
-              v_fine(fine)=(-v_minus_minus + 9*v_minus
-                            + 9*v_plus - v_plus_plus)/16;
+              v_coarse=(-v(center+jp_s-ip) + 9*v(center+jp_s)
+                        + 9*v(center+jp_s+ip) - v(center+jp_s+ip+ip))/16;
             }
+          v_fine(fine)=(8*v_coarse + 10*v_fine(fine-jp_s)
+                        - 3*v_fine(fine-jp_s-jp_s))/15;
         }
     }
 }
