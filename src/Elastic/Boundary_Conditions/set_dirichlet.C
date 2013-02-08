607160f2c05f9f4a5bd2d18b8803d4cf9bbf42dc
Walter Landry <wlandry@caltech.edu>
2013-02-08 12:01:15 -0800

    Change a name of an iterator

--- src/Elastic/Boundary_Conditions/set_dirichlet.C
+++ src/Elastic/Boundary_Conditions/set_dirichlet.C
@@ -18,51 +18,51 @@ void Elastic::Boundary_Conditions::set_dirichlet
     {
       double offset[]={0.5,0.5,0.5};
       offset[ix]=0;
-      SAMRAI::pdat::SideIterator send(gbox,ix,false);
-      for(SAMRAI::pdat::SideIterator si(gbox,ix,true); si!=send; si++)
+      SAMRAI::pdat::SideIterator s_end(gbox,ix,false);
+      for(SAMRAI::pdat::SideIterator si(gbox,ix,true); si!=s_end; si++)
         {
-          SAMRAI::pdat::SideIndex x(*si);
+          SAMRAI::pdat::SideIndex s(*si);
               
           for(int d=0;d<dim;++d)
             coord[d]=geom->getXLower()[d]
-              + dx[d]*(x[d]-pbox.lower()[d]+offset[d]);
+              + dx[d]*(s[d]-pbox.lower()[d]+offset[d]);
 
           /* For normal BC's, for the point just outside the
              boundary, set a sentinel value for normal dirichlet
              BC or the derivative for normal traction BC. */
-          if(x[ix]<pbox.lower(ix) && geom->getTouchesRegularBoundary(ix,0))
+          if(s[ix]<pbox.lower(ix) && geom->getTouchesRegularBoundary(ix,0))
             {
               if(is_dirichlet[ix][ix][0])
-                v(x)=boundary_value;
+                v(s)=boundary_value;
             }
-          else if(x[ix]>pbox.upper(ix)+1
+          else if(s[ix]>pbox.upper(ix)+1
                   && geom->getTouchesRegularBoundary(ix,1))
             {
               if(is_dirichlet[ix][ix][1])
-                v(x)=boundary_value;
+                v(s)=boundary_value;
             }
           /* If at the boundary line, set values for normal
            * components. */
-          else if(x[ix]==pbox.lower(ix)
+          else if(s[ix]==pbox.lower(ix)
                   && geom->getTouchesRegularBoundary(ix,0))
             {
               if(is_dirichlet[ix][ix][0])
                 {
                   if(!homogeneous)
-                    v(x)=expression[ix][ix][0].eval(coord);
+                    v(s)=expression[ix][ix][0].eval(coord);
                   else
-                    v(x)=0;
+                    v(s)=0;
                 }
             }
-          else if(x[ix]==pbox.upper(ix)+1
+          else if(s[ix]==pbox.upper(ix)+1
                   && geom->getTouchesRegularBoundary(ix,1))
             {
               if(is_dirichlet[ix][ix][1])
                 {
                   if(!homogeneous)
-                    v(x)=expression[ix][ix][1].eval(coord);
+                    v(s)=expression[ix][ix][1].eval(coord);
                   else
-                    v(x)=0;
+                    v(s)=0;
                 }
             }
           /* Set tangential components. */
@@ -70,29 +70,29 @@ void Elastic::Boundary_Conditions::set_dirichlet
             {
               for(int iy=(ix+1)%dim; iy!=ix; iy=(iy+1)%dim)
                 {
-                  if(x[iy]<pbox.lower(iy)
+                  if(s[iy]<pbox.lower(iy)
                      && geom->getTouchesRegularBoundary(iy,0))
                     {
                       double coord_save(geom->getXLower()[iy]);
                       std::swap(coord[iy],coord_save);
                       if(is_dirichlet[ix][iy][0])
                         {
-                          v(x)=-v(x+pp[iy]);
+                          v(s)=-v(s+pp[iy]);
                           if(!homogeneous)
-                            v(x)+=2*expression[ix][iy][0].eval(coord);
+                            v(s)+=2*expression[ix][iy][0].eval(coord);
                         }
                       std::swap(coord[iy],coord_save);
                     }
-                  else if(x[iy]>pbox.upper(iy)
+                  else if(s[iy]>pbox.upper(iy)
                           && geom->getTouchesRegularBoundary(iy,1))
                     {
                       double coord_save(geom->getXUpper()[iy]);
                       std::swap(coord[iy],coord_save);
                       if(is_dirichlet[ix][iy][1])
                         {
-                          v(x)=-v(x-pp[iy]);
+                          v(s)=-v(s-pp[iy]);
                           if(!homogeneous)
-                            v(x)+=2*expression[ix][iy][1].eval(coord);
+                            v(s)+=2*expression[ix][iy][1].eval(coord);
                         }
                       std::swap(coord[iy],coord_save);
                     }
