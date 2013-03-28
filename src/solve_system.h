b01ad9000e4affc8becf92b3b9c666c44beb6fe1
Walter Landry <wlandry@caltech.edu>
2013-03-28 13:10:34 -0700

    Fix logic for when to stop simulation

--- src/solve_system.h
+++ src/solve_system.h
@@ -74,7 +74,7 @@ void solve_system(T &fac,
       SAMRAI::tbox::plog << "Newly adapted hierarchy\n";
 
       done = !(patch_hierarchy->finerLevelExists(lnum));
-      done=(!fac.solve() && done);
+      done=(!fac.solve() || done);
     }
   if (use_visit)
     visit_writer->writePlotData(patch_hierarchy, lnum);
