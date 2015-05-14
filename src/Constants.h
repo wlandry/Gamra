0951cc3211ac9da339a507811a0c6b8bd8d92b03
Walter Landry <wlandry@caltech.edu>
2015-05-13 22:53:48 -0700

    Make index_map use Gamra::Dir

--- src/Constants.h
+++ src/Constants.h
@@ -5,17 +5,23 @@
 #define GAMRA_CONSTANTS_H
 
 #include <limits>
+#include "Dir.h"
+
 const double boundary_value=1.1111e100;
 const double invalid_value=3.1415926*boundary_value;
 const int invalid_id=-1;
-inline int index_map(const int &ix, const int &iy, const int &dim)
+inline Gamra::Dir index_map(const Gamra::Dir &ix,
+                            const Gamra::Dir &iy,
+                            const int &dim)
 {
   if(dim==2)
     return 0;
 
-  const int index[3][3]={{std::numeric_limits<int>::max(),0,2},
-                         {2,std::numeric_limits<int>::max(),0},
-                         {0,2,std::numeric_limits<int>::max()}};
+  const Gamra::Dir max=std::numeric_limits<Gamra::Dir>::max();
+  const Gamra::Dir
+    index[3][3]={{max,0,2},
+                 {2,max,0},
+                 {0,2,max}};
   return index[ix][iy];
 }
 #endif
