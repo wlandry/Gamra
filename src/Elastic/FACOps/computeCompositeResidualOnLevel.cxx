8c0a1d87fe64bed557399d522424f6c98bae522d
Walter Landry <wlandry@caltech.edu>
2016-02-21 11:47:49 -0800

    Cleanup

--- src/Elastic/FACOps/computeCompositeResidualOnLevel.cxx
+++ src/Elastic/FACOps/computeCompositeResidualOnLevel.cxx
@@ -14,62 +14,25 @@ void Elastic::FACOps::computeCompositeResidualOnLevel
 {
   t_compute_composite_residual->start();
 
-#ifdef DEBUG_CHECK_ASSERTIONS
-  if (residual.getPatchHierarchy() != d_hierarchy
-      || solution.getPatchHierarchy() != d_hierarchy
-      || rhs.getPatchHierarchy() != d_hierarchy) {
-    TBOX_ERROR(d_object_name << ": Vector hierarchy does not match\n"
-               "internal hierarchy.");
-  }
-#endif
-  boost::shared_ptr<SAMRAI::hier::PatchLevel>
-    level = d_hierarchy->getPatchLevel(ln);
+  if (residual.getPatchHierarchy() != solution.getPatchHierarchy()
+      || rhs.getPatchHierarchy() != solution.getPatchHierarchy())
+    TBOX_ERROR(d_object_name << ": residual, solution, and rhs hierarchies "
+               << "are not consistent.");
 
-  /*
-   * Set up the bc helper so that when we use a refine schedule
-   * to fill ghosts, the correct data is operated on.
-   */
+  boost::shared_ptr<SAMRAI::hier::PatchLevel>
+    level = solution.getPatchHierarchy()->getPatchLevel(ln);
+
   const int v_id = solution.getComponentDescriptorIndex(0);
   v_refine_patch_strategy.data_id=v_id;
   v_refine_patch_strategy.is_residual=error_equation_indicator;
   V_Boundary_Refine::is_residual=error_equation_indicator;
 
-  /*
-   * Assumptions:
-   * 1. Data does not yet exist in ghost boundaries.
-   * 2. Residual data on next finer grid (if any)
-   *    has been computed already.
-   * 3. Flux data from next finer grid (if any) has
-   *    been computed but has not been coarsened to
-   *    this level.
-   *
-   * Steps:
-   * S1. Fill solution ghost data by refinement
-   *     or setting physical boundary conditions.
-   *     This also brings in information from coarser
-   *     to form the composite grid flux.
-   * S2. Compute flux on ln.
-   * S3. If next finer is available,
-   *     Coarsen flux data on next finer level,
-   *     overwriting flux computed from coarse data.
-   * S4. Compute residual data from flux.
-   */
-
-  /* S1. Fill solution ghost data. */
-
-  if (ln > d_ln_min) {
-    /* Fill from current, next coarser level and physical boundary */
+  if (ln > d_ln_min)
     xeqScheduleGhostFill(v_id, ln);
-  } else {
-    /* Fill from current and physical boundary */
+  else
     xeqScheduleGhostFillNoCoarse(v_id, ln);
-  }
 
   set_boundaries(v_id,ln,error_equation_indicator);
-
-  /*
-   * S4. Compute residual on patches in level.
-   */
 
   for (SAMRAI::hier::PatchLevel::Iterator pi(level->begin());
        pi!=level->end(); ++pi)
