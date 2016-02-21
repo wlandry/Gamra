64d3902c81afdd9aaa852d9dbb4ac8d112c2a590
Walter Landry <wlandry@caltech.edu>
2016-02-20 20:54:27 -0800

    Simplify

--- src/Elastic/FACSolver/deallocateSolverState.cxx
+++ src/Elastic/FACSolver/deallocateSolverState.cxx
@@ -1,41 +1,21 @@
-/*************************************************************************
- *
- * This file is part of the SAMRAI distribution.  For full copyright 
- * information, see COPYRIGHT and COPYING.LESSER. 
- *
- * Copyright:     (c) 1997-2010 Lawrence Livermore National Security, LLC
- * Description:   High-level solver (wrapper) for scalar Elastic equation. 
- *
- ************************************************************************/
-#include <SAMRAI/pdat/CellVariable.h>
-#include "Elastic/FACSolver.hxx"
-#include <SAMRAI/tbox/PIO.h>
-#include <SAMRAI/tbox/Utilities.h>
-#include <SAMRAI/tbox/StartupShutdownManager.h>
+/// Copyright: (c) 1997-2010 Lawrence Livermore National Security, LLC
+/// Copyright: (c) 2013-2016 California Institute of Technology
 
-#include IOMANIP_HEADER_FILE
+#include "Elastic/FACSolver.hxx"
 
 void Elastic::FACSolver::deallocateSolverState()
 {
-  if (d_hierarchy) {
+  if (d_hierarchy)
+    {
+      d_fac_precond.deallocateSolverState();
+      for (int ln = d_ln_min; ln <= d_ln_max; ++ln)
+        d_hierarchy->getPatchLevel(ln)->deallocatePatchData
+          (s_weight_id[d_dim.getValue() - 1]);
 
-    d_fac_precond.deallocateSolverState();
-
-    /*
-     * Delete internally managed data.
-     */
-    int ln;
-    for (ln = d_ln_min; ln <= d_ln_max; ++ln) {
-      d_hierarchy->getPatchLevel(ln)->deallocatePatchData(s_weight_id[d_dim.getValue()
-                                                                      - 1]);
+      d_hierarchy.reset();
+      d_ln_min = -1;
+      d_ln_max = -1;
+      d_solver_is_initialized = false;
+      destroyVectorWrappers();
     }
-
-    d_hierarchy.reset();
-    d_ln_min = -1;
-    d_ln_max = -1;
-    d_solver_is_initialized = false;
-
-    destroyVectorWrappers();
-
-  }
 }
