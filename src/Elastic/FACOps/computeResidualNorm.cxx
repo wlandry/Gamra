d23e04b9a36d7b1fc8c5a2d7db176c168970f1cc
Walter Landry <wlandry@caltech.edu>
2016-02-22 12:12:50 -0800

    Add a missing copyright notice

--- src/Elastic/FACOps/computeResidualNorm.cxx
+++ src/Elastic/FACOps/computeResidualNorm.cxx
@@ -1,3 +1,7 @@
+/// Copyright © 1997-2010 Lawrence Livermore National Security, LLC
+/// Copyright © 2013-2016 California Institute of Technology
+/// Copyright © 2013-2016 Nanyang Technical University
+
 #include "Elastic/FACOps.hxx"
 
 double Elastic::FACOps::computeResidualNorm
