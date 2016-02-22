b6e67fb8f4191961b0547fa73f9d992bed12eb31
Walter Landry <wlandry@caltech.edu>
2016-02-22 15:55:31 -0800

    Cleanup

--- src/Elastic/FACOps/solveCoarsestLevel.cxx
+++ src/Elastic/FACOps/solveCoarsestLevel.cxx
@@ -1,48 +1,8 @@
-/*************************************************************************
- *
- * This file is part of the SAMRAI distribution.  For full copyright 
- * information, see COPYRIGHT and COPYING.LESSER. 
- *
- * Copyright:     (c) 1997-2010 Lawrence Livermore National Security, LLC
- * Description:   Operator class for cell-centered scalar Elastic using FAC 
- *
- ************************************************************************/
+/// Copyright © 1997-2010 Lawrence Livermore National Security, LLC
+/// Copyright © 2013-2016 California Institute of Technology
+/// Copyright © 2013-2016 Nanyang Technical University
+
 #include "Elastic/FACOps.hxx"
-
-#include IOMANIP_HEADER_FILE
-
-#include <SAMRAI/hier/BoundaryBoxUtils.h>
-#include <SAMRAI/geom/CartesianGridGeometry.h>
-#include <SAMRAI/geom/CartesianPatchGeometry.h>
-#include <SAMRAI/hier/Index.h>
-#include <SAMRAI/hier/Variable.h>
-#include <SAMRAI/hier/VariableDatabase.h>
-#include <SAMRAI/pdat/CellDoubleConstantRefine.h>
-#include <SAMRAI/pdat/CellVariable.h>
-#include <SAMRAI/pdat/OutersideData.h>
-#include <SAMRAI/pdat/OutersideVariable.h>
-#include <SAMRAI/hier/PatchData.h>
-#include <SAMRAI/pdat/SideVariable.h>
-#include <SAMRAI/solv/FACPreconditioner.h>
-#include <SAMRAI/tbox/Array.h>
-#include <SAMRAI/tbox/MathUtilities.h>
-#include <SAMRAI/tbox/StartupShutdownManager.h>
-#include <SAMRAI/tbox/Timer.h>
-#include <SAMRAI/tbox/TimerManager.h>
-#include <SAMRAI/tbox/Utilities.h>
-#include <SAMRAI/tbox/MathUtilities.h>
-#include <SAMRAI/xfer/CoarsenAlgorithm.h>
-#include <SAMRAI/xfer/CoarsenSchedule.h>
-#include <SAMRAI/xfer/RefineAlgorithm.h>
-#include <SAMRAI/xfer/RefineSchedule.h>
-#include <SAMRAI/xfer/PatchLevelFullFillPattern.h>
-
-/*
-********************************************************************
-* FACOperatorStrategy virtual solveCoarsestLevel             *
-* function                                                         *
-********************************************************************
-*/
 
 int Elastic::FACOps::solveCoarsestLevel
 (SAMRAI::solv::SAMRAIVectorReal<double>& data,
@@ -51,18 +11,11 @@ int Elastic::FACOps::solveCoarsestLevel
 {
   t_solve_coarsest->start();
 
-  int return_value = 0;
-
   d_residual_tolerance_during_smoothing = d_coarse_solver_tolerance;
-  smoothError(data,
-              residual,
-              coarsest_ln,
-              d_coarse_solver_max_iterations);
+  smoothError(data, residual, coarsest_ln, d_coarse_solver_max_iterations);
   d_residual_tolerance_during_smoothing = -1.0;
-
   xeqScheduleGhostFillNoCoarse(data.getComponentDescriptorIndex(0),
                                coarsest_ln);
   t_solve_coarsest->stop();
-
-  return return_value;
+  return 0;
 }
