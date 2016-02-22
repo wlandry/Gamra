7e50e49ed3411931cc83c5d9b708d617abdf2187
Walter Landry <wlandry@caltech.edu>
2016-02-22 13:22:28 -0800

    Cleanup

--- src/Elastic/FACOps/restrictResidual.cxx
+++ src/Elastic/FACOps/restrictResidual.cxx
@@ -1,47 +1,8 @@
-/*************************************************************************
- *
- * This file is part of the SAMRAI distribution.  For full copyright 
- * information, see COPYRIGHT and COPYING.LESSER. 
- *
- * Copyright:     (c) 1997-2010 Lawrence Livermore National Security, LLC
- * Description:   Operator class for cell-centered scalar Elastic using FAC 
- *
- ************************************************************************/
+/// Copyright © 1997-2010 Lawrence Livermore National Security, LLC
+/// Copyright © 2013-2016 California Institute of Technology
+/// Copyright © 2013-2016 Nanyang Technical University
+
 #include "Elastic/FACOps.hxx"
-
-#include IOMANIP_HEADER_FILE
-
-#include <SAMRAI/hier/BoundaryBoxUtils.h>
-#include <SAMRAI/geom/CartesianGridGeometry.h>
-#include <SAMRAI/geom/CartesianPatchGeometry.h>
-#include <SAMRAI/hier/Index.h>
-#include <SAMRAI/hier/Variable.h>
-#include <SAMRAI/hier/VariableDatabase.h>
-#include <SAMRAI/pdat/CellDoubleConstantRefine.h>
-#include <SAMRAI/pdat/CellVariable.h>
-#include <SAMRAI/pdat/OutersideData.h>
-#include <SAMRAI/pdat/OutersideVariable.h>
-#include <SAMRAI/hier/PatchData.h>
-#include <SAMRAI/pdat/SideVariable.h>
-#include <SAMRAI/solv/FACPreconditioner.h>
-#include <SAMRAI/tbox/Array.h>
-#include <SAMRAI/tbox/MathUtilities.h>
-#include <SAMRAI/tbox/StartupShutdownManager.h>
-#include <SAMRAI/tbox/Timer.h>
-#include <SAMRAI/tbox/TimerManager.h>
-#include <SAMRAI/tbox/Utilities.h>
-#include <SAMRAI/tbox/MathUtilities.h>
-#include <SAMRAI/xfer/CoarsenAlgorithm.h>
-#include <SAMRAI/xfer/CoarsenSchedule.h>
-#include <SAMRAI/xfer/RefineAlgorithm.h>
-#include <SAMRAI/xfer/RefineSchedule.h>
-#include <SAMRAI/xfer/PatchLevelFullFillPattern.h>
-
-/*
-********************************************************************
-* FACOperatorStrategy virtual restrictresidual function.     *
-********************************************************************
-*/
 
 void Elastic::FACOps::restrictResidual
 (const SAMRAI::solv::SAMRAIVectorReal<double>& s,
@@ -53,7 +14,7 @@ void Elastic::FACOps::restrictResidual
   int v_src(s.getComponentDescriptorIndex(0)),
     v_dst(d.getComponentDescriptorIndex(0));
 
-  /* Need to do a sync because the coarsening for v uses ghost zones */
+  /// Need to do a sync because the coarsening for v uses ghost zones
   v_coarsen_patch_strategy.data_id=v_src;
   v_coarsen_patch_strategy.is_residual=true;
   xeqScheduleGhostFillNoCoarse(v_src,dest_ln+1);
