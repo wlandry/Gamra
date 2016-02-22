79790cbd58b928b8e321cbdb11a451cf9c79d96a
Walter Landry <wlandry@caltech.edu>
2016-02-22 12:50:14 -0800

    Cleanup

--- src/Elastic/FACOps/xeqScheduleProlongation.cxx
+++ src/Elastic/FACOps/xeqScheduleProlongation.cxx
@@ -1,21 +1,14 @@
-/*************************************************************************
- *
- * This file is part of the SAMRAI distribution.  For full copyright 
- * information, see COPYRIGHT and COPYING.LESSER. 
- *
- * Copyright:     (c) 1997-2010 Lawrence Livermore National Security, LLC
- * Description:   Operator class for cell-centered scalar Elastic using FAC 
- *
- ************************************************************************/
+/// Copyright © 1997-2010 Lawrence Livermore National Security, LLC
+/// Copyright © 2013-2016 California Institute of Technology
+/// Copyright © 2013-2016 Nanyang Technical University
+
 #include "Elastic/FACOps.hxx"
 
-void Elastic::FACOps::xeqScheduleProlongation
-(int v_dst, int v_src, int v_scr, int dest_ln)
+void Elastic::FACOps::xeqScheduleProlongation (int v_dst, int v_src, int v_scr,
+                                               int dest_ln)
 {
-  /* v */
-  if (!v_prolongation_refine_schedules[dest_ln]) {
+  if (!v_prolongation_refine_schedules[dest_ln])
     TBOX_ERROR("Expected schedule not found.");
-  }
   SAMRAI::xfer::RefineAlgorithm refiner;
   refiner.registerRefine(v_dst, v_src, v_scr, v_prolongation_refine_operator);
   if(have_embedded_boundary())
