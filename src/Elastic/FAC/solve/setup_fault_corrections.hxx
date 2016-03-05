494148742f5164807788fb6e97ad1cdc5b04662f
Walter Landry <wlandry@caltech.edu>
2016-03-05 15:41:13 -0800

    Do iteration over faults inside compute_dv

--- src/Elastic/FAC/solve/setup_fault_corrections.hxx
+++ src/Elastic/FAC/solve/setup_fault_corrections.hxx
@@ -5,7 +5,7 @@
 
 #include "Elastic/FAC.hxx"
 
-void compute_dv(const std::vector<double> &faults, const size_t &offset,
+void compute_dv(const std::vector<double> &faults,
                 const Gamra::Dir &dim, const double *dx,
                 const double *geom_xlower,
                 const SAMRAI::hier::Box &gbox,
@@ -32,16 +32,24 @@ void Elastic::FAC::setup_fault_corrections()
       for(SAMRAI::hier::PatchLevel::Iterator p(level->begin());
           p!=level->end(); ++p)
         {
-          const boost::shared_ptr<SAMRAI::geom::CartesianPatchGeometry> &geom
-            (boost::dynamic_pointer_cast<SAMRAI::geom::CartesianPatchGeometry>
-             ((*p)->getPatchGeometry()));
-          const double *dx=geom->getDx();
-
           const boost::shared_ptr<SAMRAI::pdat::SideData<double> > &v_rhs_ptr
             (boost::dynamic_pointer_cast<SAMRAI::pdat::SideData<double> >
              ((*p)->getPatchData(v_rhs_id)));
           SAMRAI::pdat::SideData<double> &v_rhs(*v_rhs_ptr);
 
+          SAMRAI::hier::Box pbox = v_rhs.getBox();
+          SAMRAI::hier::Box gbox = v_rhs.getGhostBox();
+          const boost::shared_ptr<SAMRAI::geom::CartesianPatchGeometry> &geom
+            (boost::dynamic_pointer_cast<SAMRAI::geom::CartesianPatchGeometry>
+             ((*p)->getPatchGeometry()));
+          for(Gamra::Dir ix=0;ix<dim;++ix)
+            {
+              if(geom->getTouchesRegularBoundary(ix,0))
+                { gbox.shorten(ix,-1); }
+              if(geom->getTouchesRegularBoundary(ix,1))
+                { gbox.shorten(ix,1); }
+            }
+          const double *dx=geom->getDx();
 
           boost::shared_ptr<SAMRAI::pdat::CellData<double> > dv_diagonal_ptr =
             boost::dynamic_pointer_cast<SAMRAI::pdat::CellData<double> >
@@ -51,8 +59,9 @@ void Elastic::FAC::setup_fault_corrections()
             boost::dynamic_pointer_cast<SAMRAI::pdat::SideData<double> >
             ((*p)->getPatchData(dv_mixed_id));
           SAMRAI::pdat::SideData<double> &dv_mixed(*dv_mixed_ptr);
-          dv_diagonal.fillAll(0);
-          dv_mixed.fillAll(0);
+
+          compute_dv(faults,dim,dx,geom->getXLower(),gbox,
+                     pbox.lower(),*dv_diagonal_ptr,*dv_mixed_ptr);
 
           boost::shared_ptr<SAMRAI::pdat::CellData<double> > cell_moduli_ptr =
             boost::dynamic_pointer_cast<SAMRAI::pdat::CellData<double> >
@@ -62,24 +71,6 @@ void Elastic::FAC::setup_fault_corrections()
           boost::shared_ptr<T> edge_moduli_ptr =
             boost::dynamic_pointer_cast<T>((*p)->getPatchData(edge_moduli_id));
           T &edge_moduli(*edge_moduli_ptr);
-
-          SAMRAI::hier::Box pbox = v_rhs.getBox();
-          SAMRAI::hier::Box gbox = v_rhs.getGhostBox();
-          for(Gamra::Dir ix=0;ix<dim;++ix)
-            {
-              if(geom->getTouchesRegularBoundary(ix,0))
-                { gbox.shorten(ix,-1); }
-              if(geom->getTouchesRegularBoundary(ix,1))
-                { gbox.shorten(ix,1); }
-            }
-
-          const int params_per_fault(9);
-          for(size_t fault_index=0;fault_index<faults.size();
-              fault_index+=params_per_fault)
-            {
-              compute_dv(faults,fault_index,dim,dx,geom->getXLower(),gbox,
-                         pbox.lower(),*dv_diagonal_ptr,*dv_mixed_ptr);
-            }
 
           /// Corrections to the rhs
           for(Gamra::Dir ix=0;ix<dim;++ix)
--- src/Elastic/FAC/solve/setup_fault_corrections/compute_dv.cxx
+++ src/Elastic/FAC/solve/setup_fault_corrections/compute_dv.cxx
@@ -30,7 +30,7 @@ void compute_intersections_3D(const FTensor::Tensor1<double,3> &ntt,
                               int intersect_mixed[4],
                               int intersect_corner[4]);
 
-void compute_dv(const std::vector<double> &faults, const size_t &offset,
+void compute_dv(const std::vector<double> &faults,
                 const Gamra::Dir &dim, const double *dx,
                 const double *geom_xlower,
                 const SAMRAI::hier::Box &gbox,
@@ -38,106 +38,114 @@ void compute_dv(const std::vector<double> &faults, const size_t &offset,
                 SAMRAI::pdat::CellData<double> &dv_diagonal,
                 SAMRAI::pdat::SideData<double> &dv_mixed)
 {
-  const double *fault_ptr=faults.data()+offset;
-  const double pi=4*atan(1);
-  double scale(-fault_ptr[0]), x(fault_ptr[1]),
-    y(fault_ptr[2]), z(fault_ptr[3]),
-    L(fault_ptr[4]), W(fault_ptr[5]),
-    strike(pi/2-fault_ptr[6]*pi/180),
-    dip(fault_ptr[7]*pi/180),
-    rake(fault_ptr[8]*pi/180);
+  dv_diagonal.fillAll(0);
+  dv_mixed.fillAll(0);
 
-  const FTensor::Tensor1<double,3> center(x,y,z);
-  const FTensor::Tensor2<double,3,3>
-    rot_strike(std::cos(strike),-std::sin(strike),0,
-               std::sin(strike),std::cos(strike),0,0,0,1),
-    rot_dip(std::sin(dip),0,std::cos(dip),
-            0,1,0,
-            -std::cos(dip),0,std::sin(dip));
-
-  FTensor::Tensor2<double,3,3> rot;
-  FTensor::Index<'a',3> a;
-  FTensor::Index<'b',3> b;
-  FTensor::Index<'c',3> c;
-  rot(a,b)=rot_dip(a,c)*rot_strike(c,b);
-
-  std::vector<FTensor::Tensor1<double,3> > Dx(dim);
-  for(int d0=0;d0<dim;++d0)
+  const int params_per_fault(9);
+  for(size_t fault_index=0;fault_index<faults.size();
+      fault_index+=params_per_fault)
     {
-      Dx[d0](a)=0.0;
-      Dx[d0](d0)=dx[d0];
-    }
-  FTensor::Tensor1<double,3> slip(0,scale,0);
-  const FTensor::Tensor2<double,3,3>
-    rot_rake(1,0,0,
-             0,std::cos(rake),std::sin(rake),
-             0,-std::sin(rake),std::cos(rake));
-  FTensor::Tensor1<double,3> jump;
-  jump(c)=rot(b,c)*rot_rake(b,a)*slip(a);
+      const double *fault_ptr=faults.data()+fault_index;
+      const double pi=4*atan(1);
+      double scale(-fault_ptr[0]), x(fault_ptr[1]),
+        y(fault_ptr[2]), z(fault_ptr[3]),
+        L(fault_ptr[4]), W(fault_ptr[5]),
+        strike(pi/2-fault_ptr[6]*pi/180),
+        dip(fault_ptr[7]*pi/180),
+        rake(fault_ptr[8]*pi/180);
 
-  double fault[]={L,W};
+      const FTensor::Tensor1<double,3> center(x,y,z);
+      const FTensor::Tensor2<double,3,3>
+        rot_strike(std::cos(strike),-std::sin(strike),0,
+                   std::sin(strike),std::cos(strike),0,0,0,1),
+        rot_dip(std::sin(dip),0,std::cos(dip),
+                0,1,0,
+                -std::cos(dip),0,std::sin(dip));
 
-  for(Gamra::Dir ix=0;ix<dim;++ix)
-    {
-      double offset[]={0.5,0.5,0.5};
-      offset[ix]=0;
+      FTensor::Tensor2<double,3,3> rot;
+      FTensor::Index<'a',3> a;
+      FTensor::Index<'b',3> b;
+      FTensor::Index<'c',3> c;
+      rot(a,b)=rot_dip(a,c)*rot_strike(c,b);
 
-      SAMRAI::pdat::SideIterator
-        s_end(SAMRAI::pdat::SideGeometry::end(gbox,ix));
-      for(SAMRAI::pdat::SideIterator
-            si(SAMRAI::pdat::SideGeometry::begin(gbox,ix));
-          si!=s_end; ++si)
+      std::vector<FTensor::Tensor1<double,3> > Dx(dim);
+      for(int d0=0;d0<dim;++d0)
         {
-          const SAMRAI::pdat::SideIndex &s(*si);
+          Dx[d0](a)=0.0;
+          Dx[d0](d0)=dx[d0];
+        }
+      FTensor::Tensor1<double,3> slip(0,scale,0);
+      const FTensor::Tensor2<double,3,3>
+        rot_rake(1,0,0,
+                 0,std::cos(rake),std::sin(rake),
+                 0,-std::sin(rake),std::cos(rake));
+      FTensor::Tensor1<double,3> jump;
+      jump(c)=rot(b,c)*rot_rake(b,a)*slip(a);
 
-          FTensor::Tensor1<double,3> xyz(0,0,0);
-          for(int d=0;d<dim;++d)
+      double fault[]={L,W};
+
+      for(Gamra::Dir ix=0;ix<dim;++ix)
+        {
+          double offset[]={0.5,0.5,0.5};
+          offset[ix]=0;
+
+          SAMRAI::pdat::SideIterator
+            s_end(SAMRAI::pdat::SideGeometry::end(gbox,ix));
+          for(SAMRAI::pdat::SideIterator
+                si(SAMRAI::pdat::SideGeometry::begin(gbox,ix));
+              si!=s_end; ++si)
             {
-              xyz(d)=geom_xlower[d]
-                + dx[d]*(s[d]-pbox_lower[d]+offset[d]) - center(d);
-            }
+              const SAMRAI::pdat::SideIndex &s(*si);
 
-          /// Rotate the coordinates into the coordinates
-          /// of the fault.  So in those coordinates, if
-          /// x<0, you are on the left, and if x>0, you
-          /// are on the right.
-          FTensor::Tensor1<double,3> ntt;
-          ntt(a)=rot(a,b)*xyz(b);
-          if(dim==2)
-            {
-              int intersect, intersect_mixed[2];
-              compute_intersections_2D(ntt,xyz,rot,Dx.data(),fault,
-                                       dim,ix,intersect,
-                                       intersect_mixed);
-
-              if(gbox.contains(s))
+              FTensor::Tensor1<double,3> xyz(0,0,0);
+              for(int d=0;d<dim;++d)
                 {
-                  SAMRAI::pdat::CellIndex c(s);
-                  dv_diagonal(c,ix)+=intersect*jump(ix);
+                  xyz(d)=geom_xlower[d]
+                    + dx[d]*(s[d]-pbox_lower[d]+offset[d]) - center(d);
                 }
 
-              dv_mixed(s,0)+=intersect_mixed[0]*jump(ix);
-              dv_mixed(s,1)-=intersect_mixed[1]*jump(ix);
-            }
-          else
-            {
-              int intersect_diagonal, intersect_mixed[4],
-                intersect_corner[4];
-              compute_intersections_3D(ntt,xyz,rot,Dx.data(),fault,
-                                       dim,ix,intersect_diagonal,
-                                       intersect_mixed,
-                                       intersect_corner);
-
-              if(gbox.contains(s))
+              /// Rotate the coordinates into the coordinates
+              /// of the fault.  So in those coordinates, if
+              /// x<0, you are on the left, and if x>0, you
+              /// are on the right.
+              FTensor::Tensor1<double,3> ntt;
+              ntt(a)=rot(a,b)*xyz(b);
+              if(dim==2)
                 {
-                  SAMRAI::pdat::CellIndex c(s);
-                  dv_diagonal(c,ix)+=intersect_diagonal*jump(ix);
+                  int intersect, intersect_mixed[2];
+                  compute_intersections_2D(ntt,xyz,rot,Dx.data(),fault,
+                                           dim,ix,intersect,
+                                           intersect_mixed);
+
+                  if(gbox.contains(s))
+                    {
+                      SAMRAI::pdat::CellIndex c(s);
+                      dv_diagonal(c,ix)+=intersect*jump(ix);
+                    }
+
+                  dv_mixed(s,0)+=intersect_mixed[0]*jump(ix);
+                  dv_mixed(s,1)-=intersect_mixed[1]*jump(ix);
                 }
-
-              for(int n=0;n<4;++n)
+              else
                 {
-                  dv_mixed(s,n)+=intersect_mixed[n]*jump(ix);
-                  dv_mixed(s,n+4)+=intersect_corner[n]*jump(ix);
+                  int intersect_diagonal, intersect_mixed[4],
+                    intersect_corner[4];
+                  compute_intersections_3D(ntt,xyz,rot,Dx.data(),fault,
+                                           dim,ix,intersect_diagonal,
+                                           intersect_mixed,
+                                           intersect_corner);
+
+                  if(gbox.contains(s))
+                    {
+                      SAMRAI::pdat::CellIndex c(s);
+                      dv_diagonal(c,ix)+=intersect_diagonal*jump(ix);
+                    }
+
+                  for(int n=0;n<4;++n)
+                    {
+                      dv_mixed(s,n)+=intersect_mixed[n]*jump(ix);
+                      dv_mixed(s,n+4)+=intersect_corner[n]*jump(ix);
+                    }
                 }
             }
         }
